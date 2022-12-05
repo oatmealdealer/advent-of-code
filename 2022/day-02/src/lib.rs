bd4898c3891ebb15564eabb6d1c4102690f7c2b3
Julian Giamblanco <giamblanco.julian@gmail.com>
2022-12-04 16:41:10 -0800

    Day 2, part 1

---
+++ 2022/day-02/src/lib.rs
@@ -0,0 +1,87 @@
+use std::cmp::Ordering;
+
+#[derive(Eq, PartialEq)]
+pub enum Throw {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Throw {
+    pub const fn base_score(&self) -> u32 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+}
+
+impl From<&str> for Throw {
+    fn from(letter: &str) -> Self {
+        match letter {
+            "A" => Self::Rock,
+            "B" => Self::Paper,
+            "C" => Self::Scissors,
+            "X" => Self::Rock,
+            "Y" => Self::Paper,
+            "Z" => Self::Scissors,
+            _ => {
+                unreachable!()
+            }
+        }
+    }
+}
+
+impl Ord for Throw {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match *self {
+            Self::Rock => match *other {
+                Self::Rock => Ordering::Equal,
+                Self::Paper => Ordering::Less,
+                Self::Scissors => Ordering::Greater,
+            },
+            Self::Paper => match *other {
+                Self::Rock => Ordering::Greater,
+                Self::Paper => Ordering::Equal,
+                Self::Scissors => Ordering::Less,
+            },
+            Self::Scissors => match *other {
+                Self::Rock => Ordering::Less,
+                Self::Paper => Ordering::Greater,
+                Self::Scissors => Ordering::Equal,
+            },
+        }
+    }
+}
+
+impl PartialOrd for Throw {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+pub struct Match(Throw, Throw);
+
+impl Match {
+    pub fn score(&self) -> u32 {
+        self.1.base_score()
+            + match self.1.cmp(&self.0) {
+                Ordering::Less => 0,
+                Ordering::Equal => 3,
+                Ordering::Greater => 6,
+            }
+    }
+}
+
+pub fn process_part1(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut letters = line.split(" ");
+            let parsed_match = Match(letters.next().unwrap().into(), letters.next().unwrap().into());
+            parsed_match.score()
+        })
+        .sum::<u32>()
+        .to_string()
+}
